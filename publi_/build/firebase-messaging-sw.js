163eac73e10387f763b2b51354bfe140627bdcca
Raghunathji <partabrajp@gmail.com>
2021-04-22 22:56:44 +0500

    adding alert and prompt feature

--- publi_/build/firebase-messaging-sw.js
+++ publi_/build/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
 
 firebase.initializeApp({
-	apiKey: 'AIzaSyC73Hnk6Xo_N1JV05Nu0RAjdVjiXwwkH5M',
-	authDomain: 'fir-messaging-ed2f0.firebaseapp.com',
-	projectId: 'fir-messaging-ed2f0',
-	storageBucket: 'fir-messaging-ed2f0.appspot.com',
-	messagingSenderId: '479787942424',
-	appId: '1:479787942424:web:5c1072c66ca4b0410e396d',
+	apiKey: 'AIzaSyAUxl6DSntSJNQcdIr8kyCDL4xQBV1e9Ds',
+	authDomain: 'fir-messaging-cae85.firebaseapp.com',
+	projectId: 'fir-messaging-cae85',
+	storageBucket: 'fir-messaging-cae85.appspot.com',
+	messagingSenderId: '110904821146',
+	appId: '1:110904821146:web:5e255d68f9addbf3552623',
 });
 
 firebase.messaging();
